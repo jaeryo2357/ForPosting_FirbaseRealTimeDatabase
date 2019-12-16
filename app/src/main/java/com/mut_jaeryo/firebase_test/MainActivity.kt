efb15602cccd053f54160a37e008e07d2e992c81
jaeryo2357 <jaeryo2357@naver.com>
2019-12-16 17:44:07 +0900

    wirte a MainActivity and create a LoadActivity

--- app/src/main/java/com/mut_jaeryo/firebase_test/MainActivity.kt
+++ app/src/main/java/com/mut_jaeryo/firebase_test/MainActivity.kt
@@ -3,10 +3,16 @@ package com.mut_jaeryo.firebase_test
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    companion object
+    {
+        val TAG = "MainActivity.class"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,10 +21,23 @@ class MainActivity : AppCompatActivity() {
 
         btn_save.setOnClickListener {
             // Cloud Firestore에 저장
+            val db = FirebaseFirestore.getInstance()
+
+            val post = Post(posts_title.text.toString(),posts_content.text.toString()).getHashMap()
+
+            db.collection("posts")
+                .add(post)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "Error adding document", e)
+                }
         }
 
         btn_load.setOnClickListener {
-        //    val intent =  Intent(this,)
+            val intent =  Intent(this,LoadActivity::class.java)
+            startActivity(intent)
         }
 
     }
--- app/src/main/java/com/mut_jaeryo/firebase_test/Post.kt
+++ app/src/main/java/com/mut_jaeryo/firebase_test/Post.kt
@@ -1,3 +1,15 @@
 package com.mut_jaeryo.firebase_test
 
 data class Post(var title:String,var content:String)
+{
+
+    fun getHashMap() : HashMap<String,String>
+    {
+        val hash = hashMapOf<String,String>(
+            "title" to title,
+            "content" to content
+        )
+
+        return hash
+    }
+}
