8eced2ba255bf83a0fe05409b3602cca800f9a5e
jaeryo2357 <jaeryo2357@naver.com>
2019-12-16 17:25:45 +0900

    post 클래스 추가

--- app/src/main/java/com/mut_jaeryo/firebase_test/MainActivity.kt
+++ app/src/main/java/com/mut_jaeryo/firebase_test/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.mut_jaeryo.firebase_test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        btn_save.setOnClickListener {
+            // Cloud Firestore에 저장
+        }
+
+        btn_load.setOnClickListener {
+        //    val intent =  Intent(this,)
+        }
+
     }
+
 }
---
+++ app/src/main/java/com/mut_jaeryo/firebase_test/Post.kt
@@ -0,0 +1,3 @@
+package com.mut_jaeryo.firebase_test
+
+data class Post(var title:String,var content:String)
